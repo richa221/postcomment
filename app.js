1d6dc69f4b5ed8712b0bdbf410688da67cab988f
avijendramewada <avijendramewada@gmail.com>
2021-06-01 20:18:38 +0530

    changes  and updated index and dashboard and app.js file

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const path= require("path");
 const router = express.Router();
 const app = express();
+const bodyParser = require('body-parser')
 const port = 3000;
-const client =require("./db")
+const client =require("./db");
+const { log } = require("console");
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: true }));
+const template= path.join(__dirname,'../views')
 const query=(async () => {
   await client.connect();
   client.query(`select * from users`,(err,result)=>{
@@ -14,22 +19,21 @@ const query=(async () => {
 }) 
 query ();
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.render('index')
   });
-// router.post('/dashboard', async (req,res)=>{
-//   try {
-//     const { username, usewin } = request.body
-
-//     pool.query('INSERT INTO users (username, email) VALUES ($1, $2)', [name, email], (error, results) => {
-     
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// })
-//   router.get('dashboard',function(req,res){
-//     res.sendFile(path.join(__dirname+'/dashboard.html'));
-//   });
+  router.post('/dashboard',async(req,res)=>{
+      const p1=req.body.firstuser_name;
+      const p2=req.body.seconduser_name;
+      if(p1!=''&& p2!=''){
+        await res.render('dashboard.hbs',{
+            player1:p1,
+            player2:p2
+        });
+    }else
+    await res.render("index")
+  });
   app.use('/', router);
+  
 app.listen(port,()=>{
     console.log(`app listing port ${port}`);
 })
