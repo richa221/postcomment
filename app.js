dc1efb45ee359cb80f7f42ca1e41fc81bceae84b
avijendramewada <avijendramewada@gmail.com>
2021-06-04 15:36:50 +0530

    changes and updated all files .

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
-const express = require("express");
-const path= require("path");
+const express = require('express');
+const path = require("path");
 const router = express.Router();
 const app = express();
 const bodyParser = require('body-parser')
 const port = 3000;
-const client =require("./db");
-const { log } = require("console");
+const client = require("./db");
 app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({ extended: true }));
-const template= path.join(__dirname,'../views')
-const query=(async () => {
-  await client.connect();
-  client.query(`select * from users`,(err,result)=>{
-  if(!err){
-  console.log(result.rows);}
-  client.end();
-})
-}) 
-query ();
-router.get('/',function(req,res){
-    res.render('index')
-  });
-  router.post('/dashboard',async(req,res)=>{
-      const p1=req.body.firstuser_name;
-      const p2=req.body.seconduser_name;
-      if(p1!=''&& p2!=''){
-        await res.render('dashboard.hbs',{
-            player1:p1,
-            player2:p2
-        });
-    }else
-    await res.render("index")
-  });
-  app.use('/', router);
-  
-app.listen(port,()=>{
-    console.log(`app listing port ${port}`);
-})
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+const template = path.join(__dirname, '../views');
+router.get('/', function(req, res) {
+	res.render('index')
+});
+app.use(express.static('./'));
+app.use(bodyParser.json());
+app.post('/data', function(req, res) {
+	var p1 = req.body.param1;
+	var w1 = req.body.param3;
+	var p2 = req.body.param2;
+	var w2 = req.body.param4;
+	const query = (async() => {
+		await client.connect();
+		client.query(`INSERT INTO users(username,usewin) VALUES('${p1}',${w1}),('${p2}',${w2})`, (err, result) => {
+			if(err) {
+				console.error(err);
+				return;
+			}
+			console.log('Data insert successful');
+			client.end();
+		})
+	})
+	query();
+	res.send(req.body);
+});
+app.use('/', router);
+app.listen(port, () => {
+	console.log(`app listing port ${port}`);
+});
