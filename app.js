ed675c63be9263e45cc76a20431fe0346a01c7e3
avijendramewada <avijendramewada@gmail.com>
2021-06-09 20:04:24 +0530

    Add new file showpPlayers and updated.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-//const path = require("path");
+const path = require("path");
 const router = express.Router();
 const app = express();
 const bodyParser = require("body-parser");
@@ -9,30 +9,38 @@ app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-//const template = path.join(__dirname, "../views");
-router.get("/", function(req, res) {
+app.set("views",path.join(__dirname, "views"));
+router.get("/", function(_req, res) {
     res.render("index");
 });
 app.use(express.static("./"));
 app.use(bodyParser.json());
+// eslint-disable-next-line no-unused-vars
 app.post("/data", function(req, res) {
     var p1 = req.body.param1;
     var w1 = req.body.param3;
     var p2 = req.body.param2;
     var w2 = req.body.param4;
-    const query = (async() => {
+    const createUser = (async() => {
         await client.connect();
-        client.query(`INSERT INTO users(username,usewin) VALUES('${p1}',${w1}),('${p2}',${w2})`, (err) => {
-            if(err) {
+        client.query(`INSERT INTO users(username,userwin) VALUES('${p1}',${w1}),('${p2}',${w2})`, (err) => {
+            if(err)
                 console.error(err);
-                return;
-            }
+            res.redirect("users");
             console.log( "Data insert successful");
             client.end();
+           
         });
+        
     });
-    query();
-    res.send(req.body);
+    createUser();
+});
+router.get("/users",(req,res)=>{
+    client.connect();
+    client.query("select * from users", (err, rows) => {
+        if(err) throw err;
+        res.render("showPlayers.hbs",{items:rows.rows});
+    });
 });
 app.use("/", router);
 app.listen(port, () => {
